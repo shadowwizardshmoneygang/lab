1f0d302950b85f695e61f696531309e267720f1c
Anton <anton.kozin.mgmt@gmail.com>
2023-11-06 00:10:49 +0300

    Add deletion of all duplicates

---
+++ src/main/java/ru/penzgtu/tasks/DeletionOfAllDuplicates.java
@@ -0,0 +1,20 @@
+package ru.penzgtu.tasks;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DeletionOfAllDuplicates implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("[?] Deletion of all duplicates");
+        List<Integer> list = new LinkedList<>(Arrays.asList(1, 1, 2));
+        System.out.println("List: " + list);
+        System.out.println("Result" + new HashSet<>(list).stream().toList());
+
+        list = new LinkedList<>(Arrays.asList(1, 1, 2, 3, 3));
+        System.out.println("List: " + list);
+        System.out.println("Result" + new HashSet<>(list).stream().toList());
+    }
+}
