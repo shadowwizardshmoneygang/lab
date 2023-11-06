f797a16cf1dbf2ccc185667540a3396e06d16c9a
Anton <anton.kozin.mgmt@gmail.com>
2023-11-06 13:32:21 +0300

    Add fractional part output

---
+++ src/main/java/ru/penzgtu/tasks/FractionalPartOutput.java
@@ -0,0 +1,27 @@
+package ru.penzgtu.tasks;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FractionalPartOutput implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("[?] Fractional part output");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the numbers of list with a space (x - end): ");
+        List<Integer> numbers = new LinkedList<>();
+        while (input.hasNextInt()) {
+            numbers.add(input.nextInt());
+        }
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i) > 0 && numbers.get(i + 1) > 0) {
+                double statement = (double) numbers.get(i) / numbers.get(i + 1);
+                System.out.println("Result is " + (statement - Math.floor(statement)));
+                break;
+            }
+        }
+    }
+}
